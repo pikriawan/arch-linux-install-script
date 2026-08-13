baf59712ece098912a3374281799f7c41ed96601
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-08-13 17:36:48 +0700

    fix install.sh and add real urls

--- install.sh
+++ install.sh
@@ -117,100 +117,107 @@ mkdir -p $HOME/.tmp
 cd $HOME/.tmp
 
 if ! pacman -Qi yaru-theme &>/dev/null; then
+    if [[ ! -f yaru-theme.tar.gz ]]; then
+        curl -o yaru-theme.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/yaru-theme.tar.gz
+    fi
+
     if [[ ! -d yaru-theme ]]; then
-        mkdir yaru-theme
-        curl -o yaru-theme/PKGBUILD ...
+        tar -xvzf yaru-theme.tar.gz
     fi
 
     cd yaru-theme
-    makepkg -si --noconfirm
+    makepkg -si --needed --noconfirm
+    cd ..
 fi
 
 if ! pacman -Qi bibata-modern-classic-cursor-theme &>/dev/null; then
+    if [[ ! -f bibata-modern-classic-cursor-theme.tar.gz ]]; then
+        curl -o bibata-modern-classic-cursor-theme.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/bibata-modern-classic-cursor-theme.tar.gz
+    fi
+
     if [[ ! -d bibata-modern-classic-cursor-theme ]]; then
-        mkdir bibata-modern-classic-cursor-theme
-        curl -o bibata-modern-classic-cursor-theme/PKGBUILD ...
+        tar -xvzf bibata-modern-classic-cursor-theme.tar.gz
     fi
 
     cd bibata-modern-classic-cursor-theme
-    makepkg -si --noconfirm
+    makepkg -si --needed --noconfirm
+    cd ..
 fi
 
 # Install yay
-if ! command -v yay &>/dev/null; then
-    rm -rf yay
-    git clone https://aur.archlinux.org/yay.git
+if ! pacman -Qi yay &>/dev/null; then
+    if [[ ! -d yay ]]; then
+        git clone https://aur.archlinux.org/yay.git
+    fi
+
     cd yay
-    makepkg -si --noconfirm
-    cd $HOME/.tmp
-    rm -rf yay
+    makepkg -si --needed --noconfirm
+    cd ..
 fi
 
 # Configure npm
-mkdir -p "$HOME/.npm-global"
-npm set prefix="$HOME/.npm-global"
+mkdir -p $HOME/.npm-global
+npm set prefix=$HOME/.npm-global
 
 # Configure zsh
-if [[ ! -d "$HOME/.zsh" ]]; then
-    mkdir "$HOME/.zsh"
-    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions"
-fi
+rm -rf $HOME/.zsh
+mkdir -p $HOME/.zsh
+git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
 
 chsh -s /usr/bin/zsh
-curl -o $HOME/.zshrc ...
+curl -o $HOME/.zshrc https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/.zshrc
 
 # Copy config files
 if [[ ! -f .config.tar.gz ]]; then
-    curl -o .config.tar.gz ...
+    curl -o .config.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/.config.tar.gz
 fi
 
 if [[ ! -d .config ]]; then
-    tar -xvzf .config
+    tar -xvzf .config.tar.gz
 fi
 
 for i in .config/*; do
-    rm -r "$HOME/$i"
-    cp -r "$i" "$HOME/$i"
+    rm -rf $HOME/$i
+    cp -r $i $HOME/$i
 done
 
 # Copy opt files
 if [[ ! -f opt.tar.gz ]]; then
-    curl -o .config.tar.gz ...
+    curl -o opt.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/opt.tar.gz
 fi
 
 if [[ ! -d opt ]]; then
     tar -xvzf opt.tar.gz
 fi
 
-mkdir -p "$HOME/.local/opt"
-rm -r "$HOME/.local/opt"
-cp -r opt "$HOME/.local/opt"
+mkdir -p $HOME/.local
+rm -rf $HOME/.local/opt
+cp -r opt $HOME/.local/opt
 
 # Copy bin files
 if [[ ! -f bin.tar.gz ]]; then
-    curl -o .config.tar.gz ...
+    curl -o bin.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/bin.tar.gz
 fi
 
 if [[ ! -d bin ]]; then
     tar -xvzf bin.tar.gz
 fi
 
-mkdir -p "$HOME/.local/bin"
-rm -r "$HOME/.local/bin"
-cp -r bin "$HOME/.local/bin"
-ln -sf "$HOME/.local/opt/theme/theme" "$HOME/.local/bin/theme"
-ln -sf "$HOME/.local/opt/theme/wallpaper" "$HOME/.local/bin/wallpaper"
+rm -rf $HOME/.local/bin
+cp -r bin $HOME/.local/bin
+ln -sf $HOME/.local/opt/theme/theme $HOME/.local/bin/theme
+ln -sf $HOME/.local/opt/theme/wallpaper $HOME/.local/bin/wallpaper
 
 # Configure speech-dispatcher
 spd-conf -ucn
 
 # Enable services
-sudo systemctl enable NetworkManager.service
 sudo systemctl enable bluetooth.service
 sudo systemctl enable ly@tty1.service
 
 # Cleanup
 cd $HOME
-rm -r "$HOME/.tmp"
+rm -rf $HOME/.tmp
 
 echo "Installation completed successfully"
+echo "Please restart your computer"
