51ccaeed2fd175e8001539f4aadf7e9fca9f4e44
Fikri Setiawan <fikrisetiawan2006@gmail.com>
2026-08-13 19:20:08 +0700

    add missing themes file and refactor install.sh

--- install.sh
+++ install.sh
@@ -113,12 +113,12 @@ sudo pacman -S --needed --noconfirm --asdeps \
 flatpak install -y flathub org.gnome.Showtime
 
 # Install themes
-mkdir -p $HOME/.tmp
-cd $HOME/.tmp
+mkdir -p "$HOME/.tmp"
+cd "$HOME/.tmp"
 
 if ! pacman -Qi yaru-theme &>/dev/null; then
     if [[ ! -f yaru-theme.tar.gz ]]; then
-        curl -o yaru-theme.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/yaru-theme.tar.gz
+        curl -fsSL -o yaru-theme.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/yaru-theme.tar.gz
     fi
 
     if [[ ! -d yaru-theme ]]; then
@@ -132,7 +132,7 @@ fi
 
 if ! pacman -Qi bibata-modern-classic-cursor-theme &>/dev/null; then
     if [[ ! -f bibata-modern-classic-cursor-theme.tar.gz ]]; then
-        curl -o bibata-modern-classic-cursor-theme.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/bibata-modern-classic-cursor-theme.tar.gz
+        curl -fsSL -o bibata-modern-classic-cursor-theme.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/bibata-modern-classic-cursor-theme.tar.gz
     fi
 
     if [[ ! -d bibata-modern-classic-cursor-theme ]]; then
@@ -156,21 +156,21 @@ if ! pacman -Qi yay &>/dev/null; then
 fi
 
 # Configure npm
-mkdir -p $HOME/.npm-global
-npm set prefix=$HOME/.npm-global
+mkdir -p "$HOME/.npm-global"
+npm set prefix="$HOME/.npm-global"
 
 # Configure zsh
-if [[ ! -d $HOME/.zsh ]]; then
-    mkdir -p $HOME/.zsh
-    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+if [[ ! -d "$HOME/.zsh" ]]; then
+    mkdir -p "$HOME/.zsh"
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$HOME/.zsh/zsh-autosuggestions"
 fi
 
 chsh -s /usr/bin/zsh
-curl -o $HOME/.zshrc https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/.zshrc
+curl -fsSL -o "$HOME/.zshrc" https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/.zshrc
 
 # Copy config files
 if [[ ! -f .config.tar.gz ]]; then
-    curl -o .config.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/.config.tar.gz
+    curl -fsSL -o .config.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/.config.tar.gz
 fi
 
 if [[ ! -d .config ]]; then
@@ -178,36 +178,48 @@ if [[ ! -d .config ]]; then
 fi
 
 for i in .config/*; do
-    rm -rf $HOME/$i
-    cp -r $i $HOME/$i
+    rm -rf "$HOME/$i"
+    cp -r $i "$HOME/$i"
 done
+
+# Copy theme files
+if [[ ! -f themes.tar.gz ]]; then
+    curl -fsSL -o themes.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/heads/main/themes.tar.gz
+fi
+
+if [[ ! -d themes ]]; then
+    tar -xvzf themes.tar.gz
+fi
+
+mkdir -p "$HOME/.local/share"
+rm -rf "$HOME/.local/share/themes"
+cp -r themes "$HOME/.local/share/themes"
 
 # Copy opt files
 if [[ ! -f opt.tar.gz ]]; then
-    curl -o opt.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/opt.tar.gz
+    curl -fsSL -o opt.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/opt.tar.gz
 fi
 
 if [[ ! -d opt ]]; then
     tar -xvzf opt.tar.gz
 fi
 
-mkdir -p $HOME/.local
-rm -rf $HOME/.local/opt
-cp -r opt $HOME/.local/opt
+rm -rf "$HOME/.local/opt"
+cp -r opt "$HOME/.local/opt"
 
 # Copy bin files
 if [[ ! -f bin.tar.gz ]]; then
-    curl -o bin.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/bin.tar.gz
+    curl -fsSL -o bin.tar.gz https://github.com/pikriawan/arch-linux-install-script/raw/refs/heads/main/bin.tar.gz
 fi
 
 if [[ ! -d bin ]]; then
     tar -xvzf bin.tar.gz
 fi
 
-rm -rf $HOME/.local/bin
-cp -r bin $HOME/.local/bin
-ln -sf $HOME/.local/opt/theme/theme $HOME/.local/bin/theme
-ln -sf $HOME/.local/opt/theme/wallpaper $HOME/.local/bin/wallpaper
+rm -rf "$HOME/.local/bin"
+cp -r bin "$HOME/.local/bin"
+ln -sf "$HOME/.local/opt/theme/theme" "$HOME/.local/bin/theme"
+ln -sf "$HOME/.local/opt/theme/wallpaper" "$HOME/.local/bin/wallpaper"
 
 # Configure speech-dispatcher
 spd-conf -ucn
@@ -217,8 +229,8 @@ sudo systemctl enable bluetooth.service
 sudo systemctl enable ly@tty1.service
 
 # Cleanup
-cd $HOME
-rm -rf $HOME/.tmp
+cd "$HOME"
+rm -rf "$HOME/.tmp"
 
 echo "Installation completed successfully"
 echo "Please restart your computer"
